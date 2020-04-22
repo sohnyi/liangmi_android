389e7b103e1c3cc4a07e585b851572002f0f687c
sohnyi <sohnyi@163.com>
2020-04-22 21:52:44 +0800

    fix(splash/SplashActivity.kt): 修正协程使用问题

--- app/src/main/java/com/sohnyi/liangmi/splash/SplashActivity.kt
+++ app/src/main/java/com/sohnyi/liangmi/splash/SplashActivity.kt
@@ -2,26 +2,36 @@ package com.sohnyi.liangmi.splash
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.sohnyi.liangmi.MainActivity
 import kotlinx.coroutines.*
-import kotlin.coroutines.coroutineContext
+import kotlin.concurrent.thread
 
 class SplashActivity: AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        wait3SecondToMain()
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUI()
+            waitToMain(1000)
+        }
     }
 
-    private fun wait3SecondToMain() {
+    private fun hideSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+    }
+
+    private fun waitToMain(time: Long = 3000) {
         GlobalScope.launch(Dispatchers.Main) {
-            delay(3000)
+            withContext(Dispatchers.Default) {
+                delay(time)
+            }
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish()
         }
 
     }
-
-
 }
