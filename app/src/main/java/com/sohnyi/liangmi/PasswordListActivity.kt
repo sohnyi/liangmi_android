c2d9ffc521cfccbc298a3a6ecf1cd3a11c6d4e98
sohnyi <sohnyi@163.com>
2023-03-26 04:20:47 +0800

    merge kotlin core 1.7 to 1.8.10

--- app/src/main/java/com/sohnyi/liangmi/PasswordListActivity.kt
+++ app/src/main/java/com/sohnyi/liangmi/PasswordListActivity.kt
@@ -108,6 +108,7 @@ class PasswordListActivity : AppCompatActivity() {
         )
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode != Activity.RESULT_OK) {
